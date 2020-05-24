16d4f923c1c1904a85c6d1a04d436d4370911c17
usami-0322 <usaco0322@gmail.com>
2020-05-24 09:53:15 +0900

    update user周り追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#top'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+  }
+  get 'about', to: 'home#about'
+  get 'policy', to: 'home#policy'
+
+  resources :users, only: [:index, :show, :edit, :update]
 end
