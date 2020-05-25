5dfe3c7b0c9af00dbb014f2d765e87d7b10a44f8
usami-0322 <usaco0322@gmail.com>
2020-05-25 10:20:43 +0900

    update flashをbootstrap適用

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,15 +1,12 @@
 module DeviseHelper
-  def devise_error_messages!
-    return '' if resource.errors.empty?
-
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    html = <<-HTML
-    <div class="alert alert-error alert-block"> <button type="button"
-    class="close" data-dismiss="alert">x</button>
-      #{messages}
-    </div>
-    HTML
-
-    html.html_safe
+  def bootstrap_alert(key)
+    case key
+    when "alert"
+      "warning"
+    when "notice"
+      "success"
+    when "error"
+      "danger"
+    end
   end
 end
