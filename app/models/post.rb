d6330ace659542767500da137132bdba2f935dd5
usami-0322 <usaco0322@gmail.com>
2020-05-27 19:22:56 +0900

    add 投稿機能

---
+++ app/models/post.rb
@@ -0,0 +1,19 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, ImageUploader
+  validates :user_id, presence: true
+  validates :content, presence: true
+  validates :title, presence: true
+  validates :field, presence: true
+  validate  :picture_size
+
+  private
+
+  # アップデートされた画像のサイズをバリデーションする
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "5MB未満にしてください")
+    end
+  end
+end
