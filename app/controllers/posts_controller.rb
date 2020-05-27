7c9c9483643cdfc0581eb3f9c7368c496ff7e511
usami-0322 <usaco0322@gmail.com>
2020-05-27 14:43:53 +0900

    add 投稿機能追加

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :correct_user, only:[:destroy]
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "投稿しました"
+      redirect_to root_url
+    else
+      @feed_itrems = []
+      render 'home/top'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "投稿を削除しました"
+    redirect_to request.referrer || root_url
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :field, :content, :picutre)
+    end
+
+    def correct_user
+      @post = current_user.post.find_by(id: params[:id])
+      redirect_to root_url if @post.nill?
+    end
+end
