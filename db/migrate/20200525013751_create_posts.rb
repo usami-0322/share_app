2657776b9c4f4af36ed6b057e076389ca8075a62
usami-0322 <usaco0322@gmail.com>
2020-05-27 19:25:37 +0900

    add post-content,field,titke追加

---
+++ db/migrate/20200525013751_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.text :content
+      t.string :field
+      t.string :title
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :posts, [:user_id, :created_at]
+  end
+end
