d74a9ac0e13b396b51e04f7442a528be0eb68f98
usami-0322 <usaco0322@gmail.com>
2020-05-27 14:43:12 +0900

    update homeサインインした人用に編集

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def top
+    if user_signed_in?
+      @post = current_user.posts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def about
