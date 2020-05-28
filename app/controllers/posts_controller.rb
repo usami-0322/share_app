37319e30eaafb9cf676d9a4e88f71e9e728d065e
usami-0322 <usaco0322@gmail.com>
2020-05-28 17:58:52 +0900

    update correct_userのonly部分詰めすぎだった

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
-  before_action :correct_user, only:[:destroy]
+  before_action :correct_user, only: [:destroy]
 
   def show
     @post = Post.find(params[:id])
@@ -25,12 +25,12 @@ class PostsController < ApplicationController
 
   private
 
-    def post_params
-      params.require(:post).permit(:title, :field, :content, :picture)
-    end
+  def post_params
+    params.require(:post).permit(:title, :field, :content, :picture)
+  end
 
-    def correct_user
-      @post = current_user.post.find_by(id: params[:id])
-      redirect_to root_url if @post.nill?
-    end
+  def correct_user
+    @post = current_user.post.find_by(id: params[:id])
+    redirect_to root_url if @post.nill?
+  end
 end
