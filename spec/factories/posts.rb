70cf41ab22aa07125763c00c1895edf840efc36a
usami-0322 <usaco0322@gmail.com>
2020-05-31 14:49:31 +0900

    update picture使用しないので削除

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     content { "MyText" }
     field { "MyString" }
     title { "MyString" }
-    picture { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/rspec_test.png')) }
     user_id { "1" }
     association :user
   end
