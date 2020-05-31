a04788c041da39d500f61829d1774ddcc23ff34c
usami-0322 <usaco0322@gmail.com>
2020-05-31 10:56:02 +0900

    update　いいね追加

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,23 @@
+class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    current_user.like(@post)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_url) }
+      format.js
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @post = Favorite.find(params[:id]).post
+    current_user.unlike(@post)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_url) }
+      format.js
+    end
+  end
+end
