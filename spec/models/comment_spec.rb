75c6dc2adedfa8a757d1298cf210ca55845b7c42
usami-0322 <usaco0322@gmail.com>
2020-06-02 15:01:56 +0900

    update let記入位置後退

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-let(:user) { create(:user) }
-let(:post) { create(:post) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
 
   it "user_idがなければ無効な状態である" do
     comment = Comment.new(user_id: nil)
