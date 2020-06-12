a311cecf5eec67336b2e8ae1c61dfabd9cb85f2e
usami-0322 <usaco0322@gmail.com>
2020-06-12 21:28:14 +0900

    update 日付期間指定変更

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,8 +1,6 @@
 Ransack.configure do |config|
-  config.add_predicate :during_year,
-                       arel_predicate: :between,
-                       formatter: proc { |x, v |
-                         Time.zone.parse("#{x.year}, #{v.month}, 1").to_date...Time.zone.parse("#{v.year + 1}, #{v.month}, -1").to_date
-                       },
-                       type: :date
+  config.add_predicate 'lteq_end_of_day',
+                        arel_predicate: 'lteq',
+                        formatter: proc {|v| v.end_of_day},
+                        compounds: false
 end
