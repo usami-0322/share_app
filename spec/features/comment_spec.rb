6e0295077878371741969851c2b0292750139225
usami-0322 <usaco0322@gmail.com>
2020-06-19 09:47:48 +0900

    update　テスト用データに変更し、書き直し

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -3,28 +3,28 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :feature do
   include Devise::Test::IntegrationHelpers
   let(:user) { create(:user) }
-  let!(:post) {create(:post, user: user) }
+  let!(:post) { create(:post, user: user) }
 
   before do
     sign_in user
   end
 
   context "ログイン後" do
-    scenario "コメントする(成功)" do
+    scenario "コメントして削除する(成功)" do
       visit post_path(post.id)
       expect(page).to have_content user.name
-      fill_in "コメントを入力して下さい", with: "コメントする"
+      fill_in "コメントを入力して下さい", with: "コメント"
       click_button "コメントする"
       expect(page).to have_content "コメントしました"
-      expect(page).to have_content "コメントする"
+      expect(page).to have_content "コメント"
+      expect { click_on "削除" }.to change(Comment, :count).by(-1)
     end
 
     scenario "コメントする(失敗)" do
       visit post_path(post.id)
       expect(page).to have_content user.name
       click_button "コメントする"
+      expect(page).to have_content "コメントを入力してください"
     end
-
-    scenario "コメント削除する"
   end
 end
