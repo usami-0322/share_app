6be4b5572b0271351d3338ab29e82672c551dcd1
usami-0322 <mldc0420@gmail.com>
2020-07-21 09:57:52 +0900

    update %i→：変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :check_gest, only: %i[update destroy]
+  before_action :check_gest, only: [:update, :destroy]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
